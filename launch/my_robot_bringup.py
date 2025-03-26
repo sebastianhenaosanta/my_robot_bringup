4b8c31f40e5220577d9c289771ba44a945e7056f
sebastianhenaosanta <sebastianhenao7@gmail.com>
2025-03-26 04:36:23 +0000

    launch .py file added

---
+++ launch/my_robot_bringup.py
@@ -0,0 +1,48 @@
+import os
+
+from launch_ros.parameter_descriptions import ParameterValue
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python import get_package_share_path
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    urdf_path = os.path.join(get_package_share_path("my_robot_description"),
+                             'urdf', 'my_robot.urdf.xacro')
+    rviz_config_path = os.path.join(get_package_share_path("my_robot_bringup"),
+                                    "rviz", "urdf_config.rviz")
+
+    robot_description = ParameterValue(Command(["xacro ", urdf_path]), value_type=str)
+
+    robot_state_publisher_node = Node(package="robot_state_publisher",
+                                      executable="robot_state_publisher",
+                                      parameters=[{'robot_description': robot_description}])
+
+    joint_state_publisher_gui_node = Node(package="joint_state_publisher_gui",
+                                          executable="joint_state_publisher_gui")
+
+    gazebo_ros = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_path("gazebo_ros"), "launch", "gazebo.launch.py")
+        )
+    )
+
+    spawn_entity =  Node(package="gazebo_ros",
+                        executable="spawn_entity.py",
+                        name="spawn_entity",
+                        output="screen",
+                        arguments=["-topic", "robot_description", "-entity", "my_robot"]
+    )
+
+    rviz2_node = Node(package="rviz2",
+                      executable="rviz2",
+                      arguments=["-d", rviz_config_path],
+                      output="screen")
+
+    return LaunchDescription([robot_state_publisher_node,
+                              joint_state_publisher_gui_node,
+                              gazebo_ros,
+                              spawn_entity,
+                              rviz2_node])
